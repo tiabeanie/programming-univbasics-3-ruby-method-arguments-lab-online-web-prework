857fc427dab87564144784b3164a049a512d2bbb
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-10-26 22:45:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -17,7 +17,8 @@ def introduction_with_language_optional(name, language=Ruby)
 end
 
 def introduction_with_language_optional(name, language)
-  puts 
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.
+
 
 
 
